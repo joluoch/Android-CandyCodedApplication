1c1df40c0c0620a1ea8c3458e2c5990f73244e56
joluoch <jakindaodhiambo@gmail.com>
2019-10-26 15:19:44 +0300

    completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -101,10 +101,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        Intent infoIntent = new Intent(this,InfoActivity.class);
-
+        Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
-
         return super.onOptionsItemSelected(item);
     }
 }
